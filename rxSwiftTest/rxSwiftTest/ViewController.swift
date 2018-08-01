5cdff24748a0b539b324d6934ffaf68bdd61c159
Gyuchan Jeon <gyuchan@lezhin.com>
2018-08-01 15:05:28 +0900

    initialization project

---
+++ rxSwiftTest/rxSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rxSwiftTest
+//
+//  Created by Gyuchan Jeon on 2018. 8. 1..
+//  Copyright © 2018년 Gyuchan Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
